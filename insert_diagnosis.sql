1822c793f04966b82f263290038f4b10d487df2a
Brandon Chin <icefence@me.com>
2018-04-21 18:42:48 -0400

    10 patients, and made some double quotes into single quotes

--- insert_diagnosis.sql
+++ insert_diagnosis.sql
@@ -1,5 +1,5 @@
 insert into diagnosis values
-("S72.309A", 123456789, "2018-03-04", null), ("314.1", 123452357, "2018-02-03", null),
-("I97.3", 123204357, "2018-01-04", "Hypertension after open heart surgery");
+('S72.309A', 123456789, '2018-03-04', null), ('314.1', 123452357, '2018-02-03', null),
+('I97.3', 123204357, '2018-01-04', 'Hypertension after open heart surgery');
 
 describe diagnosis;
--- insert_patients.sql
+++ insert_patients.sql
@@ -1,4 +1,5 @@
-insert into patient values(
+insert into patient values
+  (
   123456789,
   'Jonathan',
   'Justino',
@@ -12,7 +13,8 @@ insert into patient values(
   'Database Admin',
   0000000432,
   043184291321
-), (
+),
+(
   123452357,
   'Renny',
   'Young',
@@ -21,26 +23,28 @@ insert into patient values(
   8045930134,
   'Yosemnite St.',
   23333,
-  'Richmond',
-  'VA',
+  'El Paso',
+  'TX',
   'Teacher',
   0000000433,
   043184243927
-), (
+),
+(
   123204357,
   'Kirk',
   null,
   'Angelo',
-  '1992-5-10',
+  '1992-05-10',
   8041020134,
   'Bergeron St.',
   23333,
-  'Richmond',
-  'VA',
+  'Albany',
+  'NY',
   'Police Officer',
   0000000433,
   043184243927
-), (
+),
+(
   923416789,
   'Sivesto',
   'Theon',
@@ -49,11 +53,101 @@ insert into patient values(
   8049214234,
   'Pie St.',
   23333,
-  'Richmond',
-  'VA',
+  'Seattle',
+  'WA',
   'Cashier',
   0000000633,
   043184291321
+),
+(
+  923413219,
+  'Harry',
+  'Winifreed',
+  'Wanker',
+  '1970-10-05',
+  8043910234,
+  'Wellington St.',
+  23333,
+  'Fairfax',
+  'VA',
+  'Soccer Player',
+  0000005343,
+  043184243927
+),
+(
+  123829359,
+  'Mariazinha',
+  'Simonetta',
+  'Neville',
+  '1949-05-01',
+  8045948534,
+  'Elder St.',
+  10394,
+  'San Jose',
+  'CA',
+  'Nurse',
+  0000000532,
+  043184291321
+),
+(
+  123413548,
+  'Elfriede',
+  'Clotho',
+  'Otten',
+  '2007-08-20',
+  8043842134,
+  'Prussian St.',
+  32932,
+  'Annapolis',
+  'MD',
+  'Student',
+  0000000433,
+  043184243927
+),
+(
+  19302357,
+  'Artemis',
+  'Minakshi',
+  'Holst',
+  '1956-12-22',
+  8041023029,
+  'Deedington St.',
+  03921,
+  'St. Louis',
+  'MI',
+  'Architect',
+  0000000433,
+  043184243927
+),
+(
+  923416789,
+  'Shabnam',
+  'Magdalena',
+  'Logan',
+  '1984-02-04',
+  8043827234,
+  'Delhi St.',
+  30291,
+  'Columbus',
+  'OH',
+  'Economist',
+  0000000633,
+  043184291321
+),
+(
+  932847219,
+  'Annamaria',
+  'Eva',
+  'Barbieri',
+  '1969-09-30',
+  8049328234,
+  'Maine St.',
+  47283,
+  'Queens',
+  'NY',
+  'Unemployyed',
+  0000005343,
+  043184243927
 );
 
 describe patient;
